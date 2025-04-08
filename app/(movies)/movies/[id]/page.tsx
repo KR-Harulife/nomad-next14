e1b06ba8b77917dfd08b20670981778968474815
HaruLife <9105dud@gmail.com>
2025-04-08 15:51:24 +0900

    test

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,11 +2,12 @@ import { Suspense } from "react";
 import MovieInfo from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export default async function MovieDetail({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+type IParams = Promise<{ id: string }>;
+
+export default async function MovieDetail(props: { params: IParams }) {
+  const params = await props.params;
+  const { id } = params;
+
   return (
     <div>
       <Suspense fallback={<h2>Loading movie info...</h2>}>
