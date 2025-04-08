137154a31527ee85eebf49d2424186ff9ec8489c
HaruLife <9105dud@gmail.com>
2025-04-08 15:31:05 +0900

    Test

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Movie from "../../components/movie";
+import { API_URL } from "../constants";
 
 export const metadata = {
   title: "Home",
 };
 
-export const API_URL =
-  "https://nomad-movies.nomadcoders.workers.dev/movies";
+
 
 // async function getMovies() {
 //   return fetch(URL).then((response) => response.json());
