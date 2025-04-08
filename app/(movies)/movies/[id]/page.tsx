73efd774899128c0034334f1a044452755c77286
HaruLife <9105dud@gmail.com>
2025-04-08 11:39:30 +0900

    test

---
+++ app/(movies)/movies/[id]/page.tsx
@@ -0,0 +1,20 @@
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+
+export default async function MovieDetail({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  return (
+    <div>
+      <Suspense fallback={<h2>Loading movie info...</h2>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h2>Loading movie videos...</h2>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import Navigation from "../components/navigation";
+
+export const metadata = {
+  title: "Home | Next Movies",
+  description: "The best movies in the world",
+  keywords: "movies, nextjs, react",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body>
+        <Navigation />
+        {children}
+      </body>
+    </html>
+  );
+}
